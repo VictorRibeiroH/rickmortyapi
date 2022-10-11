bf44fb32ee9f01cb7609c8cc35d3a8eb1bb3fded
VictorRibeiroH <contato.victoribeiro@gmail.com>
2022-10-11 10:48:01 -0300

    paginação entre as paginas

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 import Cards from "./components/Cards/Cards";
 import Filters from "./components/Filters/Filters";
+import Pagination from "./components/Pagination/Pagination";
 
 function App() {
   let [pageNumber, setPageNumber] = useState(1);
@@ -37,6 +38,8 @@ function App() {
           </div>
         </div>
       </div>
+
+      <Pagination pageNumber={pageNumber} setPageNumber={setPageNumber} />
     </div>
   );
 }
---
+++ src/components/Pagination/Pagination.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Pagination = ({ pageNumber, setPageNumber }) => {
+  let next = () => {
+    setPageNumber((x) => x + 1);
+  };
+
+  let prev = () => {
+    if (pageNumber === 1) return;
+    setPageNumber((x) => x - 1);
+  };
+
+  return (
+    <div className="container d-flex justify-content-center gap-5 my-5">
+      <button onClick={prev} className="btn btn-primary">
+        Anterior
+      </button>
+      <button onClick={next} className="btn btn-primary">
+        Próxima
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
